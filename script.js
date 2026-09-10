97d3370f501927c1136a54a740ec80e0f2183e25
shriyabhavanasi12-cell <shriyabhavanasi.12@gmail.com>
2026-09-10 23:17:22 +0530

    add snowfall effect

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const globe = document.querySelector("#globe");
 const button = document.querySelector("#shake");
 const message = document.querySelector("#message");
 
+const snowContainer=document.getElementById("snow-container");
+const snowButton= document.getElementById("snowButton");
+
 const messages = [
   
     " The Holiday- a warm ,charming story about two women who swap homes for Christmas . ",
@@ -24,3 +27,44 @@ button.addEventListener("click", () => {
   const pick = Math.floor(Math.random() * messages.length);
   message.textContent = messages[pick];
 });
+
+let snowing=true;
+let snowflakes=[];
+for(let i=0; i< 100;i++){
+  const flake=document.createElement("div");
+
+  flake.classList.add("snowflake");
+  flake.innerHTML="❄";
+
+  flake.style.left=Math.random()*100+"vw";
+  flake.style.animationDuration=(Math.random()*5+3)+"s";
+  
+
+  flake.style.animationDelay = "0s";
+flake.style.animationPlayState = "running";
+
+  snowContainer.appendChild(flake);
+  snowflakes.push(flake);
+
+
+
+}
+
+snowButton.addEventListener("click",()=>{
+
+snowing=!snowing;
+
+snowflakes.forEach((flake)=>{
+  flake.style.animationPlayState=
+  snowing ? "running":"paused";
+
+});
+
+snowButton.textContent=
+snowing? "Stop Snow": "Start Snow";
+
+
+
+
+});
+
